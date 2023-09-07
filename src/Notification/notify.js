bb526fd489618c3ccdaee49255c439d121c7d6e6
kuberpatel <101393723+kuberpatel@users.noreply.github.com>
2023-09-07 09:50:39 +0530

    Update notify.js

--- src/Notification/notify.js
+++ src/Notification/notify.js
@@ -1,6 +1,10 @@
+// Import the 'toast' component from 'react-toastify'
 import { toast } from "react-toastify";
 
+// Define a function 'showToastMessage' that takes a 'message' and 'type' as arguments
 export const showToastMessage = (message, type) => {
+  // Use the 'toast' function to display a toast message with the provided 'message' and 'type'
+  // Position the toast message at the top-right corner of the screen
   toast[type](message, {
     position: toast.POSITION.TOP_RIGHT,
   });
