ef1005af28c93c5310a2a55c7b72066983ff17be
kuberpatel <101393723+kuberpatel@users.noreply.github.com>
2023-09-07 09:08:25 +0530

    Update Nav.js

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+// Import necessary libraries and functions
+import React, { useState } from "react"; // Import React and useState
+import { useSelector } from "react-redux"; // Import useSelector from react-redux
+import { useNavigate } from "react-router-dom"; // Import useNavigate from react-router-dom
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+
+// Define the functional component Nav
 export default function Nav() {
+  // Get the navigate function from react-router-dom
   const navigate = useNavigate();
+
+  // Get the 'totalCart' value from the Redux store using useSelector
   let total = useSelector((state) => state.totalCart);
-return (
+
+  return (
+    // Navbar component with Bootstrap styles
     <nav
       className="navbar navbar-expand-lg p-4 align-items-center"
       style={style.nav}
     >
       <div className="container-fluid">
+        {/* E-commerce brand */}
         <a className="navbar-brand fs-3" href="#" style={style.navHead}>
           E-commerce
         </a>
+        {/* Navbar toggle button for mobile */}
         <button
           className="navbar-toggler"
           type="button"
@@ -25,27 +35,33 @@ return (
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+        {/* Navbar links */}
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
+              {/* Link to the "Products" page */}
               <Link to="/" className="nav-link active text-light" href="#">
                 Products
               </Link>
             </li>
             <li className="nav-item">
+              {/* Link to the "Add a product" page */}
               <Link to="/addproducts" className="nav-link active text-light">
                 Add a product
               </Link>
             </li>
           </ul>
+          {/* Cart and notification icons */}
           <div className="d-flex gap-5 position-relative">
+            {/* Cart icon */}
             <img
               src="https://cdn-icons-png.flaticon.com/128/2838/2838694.png"
               alt="error"
               width={"40rem"}
-            onClick={() => navigate("/cart")}
-            style={{cursor:"pointer"}}
+              onClick={() => navigate("/cart")}
+              style={{ cursor: "pointer" }}
             />
+            {/* Display the total number of items in the cart if 'total' is greater than 0 */}
             {total ? (
               <p
                 className="bg-white rounded-circle position-absolute d-flex align-items-center justify-content-center"
@@ -61,6 +77,7 @@ return (
             ) : (
               ""
             )}
+            {/* Notification icon */}
             <img
               src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png"
               alt="error"
@@ -72,17 +89,8 @@ return (
     </nav>
   );
 }
-// const style = {
-//   nav: {
-//     backgroundColor: "var(--nav)",
-//   },
-//   navHead: {
-//     fontFamily: "var(--fontStyle)",
-//     color: "#f47289",
-//   },
-// };
 
-
+// Define CSS styles for the navigation component
 const style = {
   nav: {
     backgroundColor: "var(--nav)",
@@ -93,4 +101,4 @@ const style = {
   },
 };
 
-// #9375b7
+// #9375b7 
