4828931ea8f5a7e44d94cea04c38b63c14b61de1
kuberpatel <101393723+kuberpatel@users.noreply.github.com>
2023-09-07 10:03:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
+// Import React and ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Import the main CSS file
 import './index.css';
+
+// Import the main App component
 import App from './App';
-import products from './reducers/index'
+
+// Import the Redux store and the products reducer
+import products from './reducers/index';
+
+// Import Material-UI components (assuming you are using Material-UI)
 import "@mui/material"
+
+// Import Redux createStore and Provider
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
-const store=createStore(products)
 
+// Create the Redux store using the products reducer
+const store = createStore(products)
 
+// Create a root for ReactDOM to render the app into
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component wrapped with the Redux Provider
 root.render(
     <Provider store={store}>
-
         <App />
     </Provider>
 );
-
-
